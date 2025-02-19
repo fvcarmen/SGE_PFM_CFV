065bc9ca5da441536e934454046bd2e534b02208
Carmen <fverducarmen@gmail.com>
2025-02-19 17:46:44 +0000

    [UPDATE] cambio preferencia horaria de boolean a horas (a modificar que coja solo hora)

--- models/genero.py
+++ models/genero.py
@@ -5,13 +5,15 @@ class Genero(models.Model):
     _description = 'Género de contenido audiovisual'
 
     name = fields.Char(string="Nombre", required=True)  
-    preferencia_horaria = fields.Boolean(string="Preferencia horaria", default=False)
+    preferencia_horaria_inicio = fields.Datetime(string="Inicio preferencia horaria")
+    preferencia_horaria_fin = fields.Datetime(string="Fin preferencia horaria")
     anuncios_ids = fields.One2many('cine_gestion.anuncio', 'genero_id', string="Anuncio")
     eventos_ids = fields.Many2many(
         'cine_gestion.evento',
         string="Evento",
     )
 
+    
 
     _sql_constraints = [
         ('unique_name','unique(name)','El nombre del género debe ser único.')
