6e7504e6c2715aecaeeb80b3ae0742939a75469c
Carmen <fverducarmen@gmail.com>
2025-02-24 03:44:03 +0000

    [UPDATE] creacion comprobación weekday de tarifa (sin aplicar a la generación)

--- models/tarifa.py
+++ models/tarifa.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class Tarifas(models.Model):
     _name = "cine_gestion.tarifa"
@@ -6,5 +7,12 @@ class Tarifas(models.Model):
 
     name = fields.Char(string="Nombre", required=True)
     precio = fields.Float(string="Precio", required=True)
+    dia_valido = fields.Integer(string="Día aplicable (-1 a 6)", help="-1 = tarifa default, 0 = Lunes ... 6 = Domingo", default = -1)
     descuentos_ids = fields.One2many('cine_gestion.descuento', 'tarifa_id', string="Descuentos")
     sesiones_ids = fields.One2many('cine_gestion.sesion', 'tarifa_id', string = "Sesiones")
+
+    @api.constrains('dia_valido')
+    def _comprobar_dia_valido(self):
+        for record in self:
+            if record.dia_valido is None or record.dia_valido < -1 or record.dia_valido > 6:
+                raise UserError("Debes seleccionar un día válido (entre -1 y 6).")
