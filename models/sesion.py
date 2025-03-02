d7566e0e9d98a33c1e6dc6da73ed65f97d59adde
Carmen <fverducarmen@gmail.com>
2025-03-02 22:18:00 +0000

    [UPDATE] eliminacion required en fecha_fin dado que es un campo calculado

--- models/sesion.py
+++ models/sesion.py
@@ -6,7 +6,7 @@ class Sesion(models.Model):
     
     name = fields.Char(string="Nombre", compute="_compute_session_name", store=True)
     fecha_inicio = fields.Datetime(string="Fecha de Inicio", required=True, store=True)
-    fecha_fin = fields.Datetime(string="Fecha de Fin", compute="_compute_fecha_fin", required=True, store = True)
+    fecha_fin = fields.Datetime(string="Fecha de Fin", compute="_compute_fecha_fin", store = True)
     evento_id = fields.Many2one('cine_gestion.evento', string="Evento", required=True)
     duracion = fields.Integer(string="Duración (min)", compute="_compute_duracion", store=True)
     ocupacion = fields.Integer(string="Ocupación", default=0)
